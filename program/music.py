6b4bc070ae908fdad5a3aaf478ea86e19aa792f0
ANES <107684658+ANES0H@users.noreply.github.com>
2022-06-18 23:01:32 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -44,7 +44,7 @@ async def play(c: Client, m: Message):
     await m.delete()
     do = requests.get(f"https://api.telegram.org/bot5150377029:AAEI7UVQceyA32MGtzMXzjphWnrCCf-lU44/getChatMember?chat_id=@EITHON&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني \n— — — — — — — — — — — — —\n - @N_B_100 ↫ ")
+        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني \n— — — — — — — — — — — — —\n - @{UPDATES_CHANNEL} ↫ ")
     else:
         replied = m.reply_to_message
         chat_id = m.chat.id
